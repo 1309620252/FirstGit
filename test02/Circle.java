6d50e8408f437570b189dc93c9ff54f321ef12be
1309620252 <1309620252@qq.com>
2015-07-24 13:28:25 +0800

    a second test

--- test02/Circle.java
+++ test02/Circle.java
@@ -2,14 +2,12 @@ package SecondTest;
 
 public class Circle implements JM{
 	private double x;
-	private double y;
 	public Circle(double x){
 		this.x = x;
-		this.y = x;
 	}
 	@Override
 	public double getArea() {
-		return Math.PI*x*y;
+		return Math.PI*x*x;
 	}
 
 }
--- test02/Square.java
+++ test02/Square.java
@@ -2,12 +2,10 @@ package SecondTest;
 
 public class Square implements JM{
 	private double x;
-	private double y;
 	public Square(double x){
 		this.x = x;
-		this.y = x;
 	}
 	public double getArea() {
-		return x*y;
+		return x*x;
 	}
 }
